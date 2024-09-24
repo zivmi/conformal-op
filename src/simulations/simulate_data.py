ADD Call prices to simulated data. BUGFIX negative strikes due to large variance when sampling N(1,var)

src/simulations/simulate_data.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
+from scipy.stats import norm
 
-from config import * # FIXME: load configs it properly
+import sys
+sys.path.append('.')
+
+from config import * 
+from src.utils.common import d1, d2, black_scholes_price
 
 if __name__ == '__main__':
     total_num_samples = int(sum(sample_sizes)/strikes_per_S)
@@ -20,7 +25,11 @@ if __name__ == '__main__':
     # generate 'strikes_per_S' number of strikes from normal with mean S and std 0.1*S, for each S
     z = np.random.normal(
             1, # mean
-            np.sqrt(0.1), # variance of 0.1, std of sqrt(0.1)
+            # variance of 0.1 => std=sqrt(0.1)=0.316
+            # ~300 strikes will be negative
+            # the author probably used 0.1 as a variance, not std.
+            # With std=0.1, the number of negative strikes will be ~0
+            0.1, 
             total_num_samples*strikes_per_S)
 
     data['K'] = data['S']/z
@@ -33,4 +42,8 @@ if __name__ == '__main__':
     for i, _ in enumerate(sample_sizes):
         data.loc[data.index.isin(range(sum(sample_sizes[:i]), sum(sample_sizes[:i+1]))), 'sample_id'] = i
 
+    # Add the option price
+    data['C'] = black_scholes_price(data['S'], data['K'], data['r'], data['sigma'], data['tau'], option_type="call")
+
+    # Save the data to a csv file
     data.to_csv(path_to_data, index=False)